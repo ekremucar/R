92cafe9f546fb19fe6214cdcdd715ee33637635b
EKREM UCAR (Musteri Cozumleri Bsk. - Analist Programci) <E_UCAR@thynet.thy.com>
2018-02-22 17:38:13 +0200

    matrices

---
+++ section9Ekrem.R
@@ -0,0 +1,19 @@
+#section 9 Matrices
+
+m <- matrix(nrow = 2, ncol = 3)
+m
+
+attributes(m) #dim
+
+dim(m)
+
+
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m
+
+
+m <- 1:10
+m
+
+dim(m) <- c(2, 5) #assing dimension -> convert list to matrix
+m
